758be997d0345749d13cee2212ebe4113986d9d2
張建文 <chang180@gmail.com>
2024-01-29 14:07:09 +0800

    Episode 4 finished

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use App\Models\Post;
-use Illuminate\Foundation\Auth\User;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comment extends Model
 {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +15,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory(10)
+            ->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $posts = Post::factory(200)->recycle($users)
+            ->create();
+
+        $comments = Comment::factory(100)->recycle($users, $posts)->create();
+
+        $chang180 = User::factory()
+        ->has(Post::factory()->count(10))
+        ->has(Comment::factory()->count(50)->recycle($posts))
+        ->create([
+            'name' => 'chang180',
+            'email' => 'chang180@gmail.com',
         ]);
     }
 }
