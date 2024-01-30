d23e442ada1fdafe84190337c742a0074066ca7e
張建文 <chang180@gmail.com>
2024-01-30 16:20:17 +0800

    Episode 7 finished

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class PostController extends Controller
     public function index()
     {
         return inertia('Post/Index', [
-            'posts' => Post::all()->toArray(),
+            'posts' => PostResource::collection(Post::all()),
         ]);
     }
 
