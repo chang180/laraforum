0c73163fb3fee3202b611926829deab8e1f8f1b9
張建文 <chang180@gmail.com>
2024-02-06 17:46:01 +0800

    Episode 21 finished

--- resources/js/Components/Comment.vue
+++ resources/js/Components/Comment.vue
@@ -13,9 +13,23 @@
                 >By {{ comment.user.name }}
                 {{ relativeDate(comment.created_at) }} ago</span
             >
-            <div class="mt-2 text-right empty:hidden" v-if="comment.can?.delete">
+            <div
+                class="flex justify-end mt-2 space-x-3 empty:hidden"
+                v-if="comment.can?.update"
+            >
+                <form @submit.prevent="$emit('edit', comment.id)">
+                    <button
+                        class="font-mono text-xs text-blue-700 hover:font-semibold"
+                    >
+                        Edit
+                    </button>
+                </form>
                 <form @submit.prevent="$emit('delete', comment.id)">
-                    <button class="font-mono text-xs text-red-700 hover:font-semibold">Delete</button>
+                    <button
+                        class="font-mono text-xs text-red-700 hover:font-semibold"
+                    >
+                        Delete
+                    </button>
                 </form>
             </div>
         </div>
@@ -24,11 +38,9 @@
 
 <script setup>
 import { relativeDate } from "@/Utilities/date.js";
-import { router, usePage } from "@inertiajs/vue3";
 import { defineProps, computed } from "vue";
 
 const props = defineProps(["comment"]);
 
-const emit = defineEmits(["delete"]);
-
+const emit = defineEmits(["delete", "edit"]);
 </script>
--- resources/js/Pages/Post/Show.vue
+++ resources/js/Pages/Post/Show.vue
@@ -11,17 +11,18 @@
             <div class="mt-8">
                 <h2 class="text-xl font-bold">Comments</h2>
 
-                <form v-if="$page.props.auth.user" @submit.prevent="addComment" class="mt-4">
+                <form v-if="$page.props.auth.user" @submit.prevent="() => commentBeingEdit ? updateComment() : addComment()" class="mt-4">
                     <div>
                         <InputLabel for="body" class="sr-only">Comment</InputLabel>
-                        <TextareaInput v-model="commentForm.body" id="body" rows="4" placeholder="Put in some words..."/>
+                        <TextareaInput ref="commentTextareaRef" v-model="commentForm.body" id="body" rows="4" placeholder="Put in some words..."/>
                         <InputError :message="commentForm.errors.body" class="mt-2" />
                     </div>
-                    <PrimaryButton class="mt-4" :disabled="commentForm.processing">Add Comment</PrimaryButton>
+                    <PrimaryButton class="mt-4" :disabled="commentForm.processing" v-text="commentIdBeingEdited?'Update Coment':'Add Comment'"></PrimaryButton>
+                    <SecondaryButton v-if="commentIdBeingEdited" @click="cancelEditComment" class="m-4">Cancel</SecondaryButton>
                 </form>
                 <ul class="mt-4 divide-y">
                     <li v-for="comment in comments.data" :key="comment.id" class="px-2 py-4">
-                        <Comment @delete="deleteComment" :comment="comment" />
+                        <Comment @delete="deleteComment" @edit="editComment" :comment="comment" />
                     </li>
                 </ul>
                 <Pagination :meta="comments.meta" :only="['comments']" />
@@ -32,7 +33,7 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
 
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 // import { formatDistance, parseISO } from 'date-fns';
 import {relativeDate} from "@/Utilities/date.js";
 import { useForm, router } from "@inertiajs/vue3";
@@ -44,6 +45,7 @@ import TextareaInput from "@/Components/TextareaInput.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import InputError from "@/Components/InputError.vue";
+import SecondaryButton from "@/Components/SecondaryButton.vue";
 
 const props = defineProps(['post', 'comments']);
 
@@ -53,9 +55,34 @@ const commentForm = useForm({
     body: ''
 });
 
+const commentIdBeingEdited = ref(null);
+
+const commentTextareaRef = ref(null);
+
+const commentBeingEdit = computed(() => props.comments.data.find(comment => comment.id === commentIdBeingEdited.value));
+
+const editComment = (commentID) => {
+    commentIdBeingEdited.value = commentID;
+    commentForm.body = commentBeingEdit.value?.body;
+    commentTextareaRef.value?.focus();
+};
+
+const cancelEditComment = () => {
+    commentIdBeingEdited.value = null;
+    commentForm.reset();
+};
+
 const addComment = () => commentForm.post(route('posts.comments.store', props.post.id),{
     preserveScroll: true,
     onSuccess: () => commentForm.reset()
+});
+
+const updateComment = () => commentForm.put(route('comments.update', {
+    comment:commentBeingEdit.value.id,
+    page: props.comments.meta.current_page
+}), {
+    preserveScroll: true,
+    onSuccess: cancelEditComment
 });
 
 const deleteComment = (commentID) => {
